e4d960b6333fe585dcc4809bf343490ac853d337
Pedro Noda <100450891+PedroNoda@users.noreply.github.com>
2022-09-11 07:59:15 -0300

    Update ex05.java

--- LISTA-7/ex05.java
+++ LISTA-7/ex05.java
@@ -6,22 +6,23 @@ public class ex05 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int linhas = 0, k = 0;
-		
+
 		System.out.println("Informe um valor: ");
 		linhas = sc.nextInt();
 		int num = linhas;
-		int vetor[] = new int[num+1];
+		int vetor[] = new int[num + 1];
 		vetor[0] = 1;
-		
+
 		for (int i = 0; i <= num; i++) {
-            System.out.println();
-            linhas = 0;
-            for (int j = 0; j <= i; j++) {
-                k = linhas + vetor[j];
-                linhas = vetor[j];
-                vetor[j] = k;
-                System.out.print(vetor[j] + ", ");
-            }
-        }
-    }
+			System.out.println();
+			linhas = 0;
+			for (int j = 0; j <= i; j++) {
+				k = linhas + vetor[j];
+				linhas = vetor[j];
+				vetor[j] = k;
+				System.out.print(vetor[j] + ", ");
+			}
+		}
+		sc.close();
+	}
 }
